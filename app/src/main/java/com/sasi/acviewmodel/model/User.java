b4135e46d23f5fd775ce5590adf9a5ef55935895
Sasi <sasikumar@giffgaff.co.uk>
2018-01-04 22:39:48 +0000

    Created model package and added the below models. - Repo - User

---
+++ app/src/main/java/com/sasi/acviewmodel/model/User.java
@@ -0,0 +1,14 @@
+package com.sasi.acviewmodel.model;
+
+/**
+ * Created by sasikumar on 04/01/2018.
+ */
+
+public class User {
+
+    public final String login;
+
+    public User(String login) {
+        this.login = login;
+    }
+}
