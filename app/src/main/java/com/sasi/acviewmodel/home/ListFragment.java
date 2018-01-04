d57e7f1f75d4e8f79256df46d01f19a4aa2e352b
Sasi <sasikumar@giffgaff.co.uk>
2018-01-04 22:05:12 +0000

    Initial commit.

---
+++ app/src/main/java/com/sasi/acviewmodel/home/ListFragment.java
@@ -0,0 +1,10 @@
+package com.sasi.acviewmodel.home;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by sasikumar on 04/01/2018.
+ */
+
+public class ListFragment extends Fragment {
+}
---
+++ app/src/main/java/com/sasi/acviewmodel/home/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sasi.acviewmodel.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sasi.acviewmodel.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
