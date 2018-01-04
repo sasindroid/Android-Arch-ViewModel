5f8aacc5343d868d5a6d8bd0140bfad70853d1f1
Sasi <sasikumar@giffgaff.co.uk>
2018-01-04 22:30:22 +0000

    Added gradle dependencies for ButterKnife & Retrofit. Added Fragment layout and used ButterKnife to unbind the views.

--- app/src/main/java/com/sasi/acviewmodel/home/ListFragment.java
+++ app/src/main/java/com/sasi/acviewmodel/home/ListFragment.java
@@ -1,10 +1,54 @@
 package com.sasi.acviewmodel.home;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.sasi.acviewmodel.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by sasikumar on 04/01/2018.
  */
 
 public class ListFragment extends Fragment {
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recycler_view;
+
+    @BindView(R.id.tv_error)
+    TextView tv_error;
+
+    @BindView(R.id.loading_view)
+    ProgressBar loading_view;
+
+    private Unbinder unbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.screen_list, container, false);
+        unbinder = ButterKnife.bind(view);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if(unbinder != null) {
+            unbinder.unbind();
+            unbinder = null;
+        }
+    }
 }
--- app/src/main/java/com/sasi/acviewmodel/home/MainActivity.java
+++ app/src/main/java/com/sasi/acviewmodel/home/MainActivity.java
@@ -1,7 +1,7 @@
 package com.sasi.acviewmodel.home;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.sasi.acviewmodel.R;
 
@@ -11,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.screen_container, new ListFragment())
+                    .commit();
+        }
     }
 }
